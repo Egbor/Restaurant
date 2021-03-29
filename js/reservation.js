0770764bc736143848da7b6fa4d063f92bc8c0fe
Egbor <egorminsk2001@gmail.com>
2021-03-29 13:35:25 +0300

    Fixed menu block alignment

--- js/reservation.js
+++ js/reservation.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
-	console.log("Test");
     $('#name, #date, #email').keyup(function() {
         if (($('#name').val() == '') || ($('#date').val() == '') || ($('#email').val() == '')) {
             $('#submit').prop('disabled', true).addClass('form-button-disabled');
         } else {
             $('#submit').prop('disabled', false).removeClass('form-button-disabled');
         }
+    });
+    $('#submit').click(function() {
+    	alert("Message");
     });
 });
